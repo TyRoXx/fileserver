0516a689e7f812182613aaf649785cb195936c18
TyRoXx <tyroxxxx@gmail.com>
2014-08-10 23:33:22 +0200

    begin to decode the requested path

--- cli/main.cpp
+++ cli/main.cpp
@@ -6,8 +6,10 @@
 #include <silicium/sending_observable.hpp>
 #include <silicium/received_from_socket_source.hpp>
 #include <silicium/observable_source.hpp>
+#include <silicium/optional.hpp>
 #include <silicium/http/http.hpp>
 #include <boost/interprocess/sync/null_mutex.hpp>
+#include <boost/container/vector.hpp>
 
 namespace
 {
@@ -16,6 +18,95 @@ namespace
 	boost::iterator_range<char const *> to_range(response_part part)
 	{
 		return boost::make_iterator_range(part.begin, part.end);
+	}
+
+	struct content_request
+	{
+#ifdef _MSC_VER
+		std::vector
+#else
+		boost::container::vector
+#endif
+			<unsigned char> digest;
+	};
+
+	Si::optional<unsigned char> decode_ascii_hex_digit(char digit)
+	{
+		switch (digit)
+		{
+		case '0': return 0;
+		case '1': return 1;
+		case '2': return 2;
+		case '3': return 3;
+		case '4': return 4;
+		case '5': return 5;
+		case '6': return 6;
+		case '7': return 7;
+		case '8': return 8;
+		case '9': return 9;
+		case 'a': case 'A': return 10;
+		case 'b': case 'B': return 11;
+		case 'c': case 'C': return 12;
+		case 'd': case 'D': return 13;
+		case 'e': case 'E': return 14;
+		case 'f': case 'F': return 15;
+		default:
+			return Si::none;
+		}
+	}
+
+	template <class InputIterator, class OutputIterator>
+	std::pair<InputIterator, OutputIterator> decode_ascii_hex_bytes(InputIterator begin, InputIterator end, OutputIterator bytes)
+	{
+		for (;;)
+		{
+			if (begin == end)
+			{
+				break;
+			}
+			char const first_char = *begin;
+			Si::optional<unsigned char> const first = decode_ascii_hex_digit(first_char);
+			if (!first)
+			{
+				break;
+			}
+			auto const next = begin + 1;
+			if (next == end)
+			{
+				break;
+			}
+			char const second_char = *next;
+			Si::optional<unsigned char> const second = decode_ascii_hex_digit(second_char);
+			if (!second)
+			{
+				break;
+			}
+			begin = next + 1;
+			unsigned char const digit = (*first * 16) + *second;
+			*bytes = digit;
+			++bytes;
+		}
+		return std::make_pair(begin, bytes);
+	}
+
+	Si::optional<content_request> parse_request_path(std::string const &path)
+	{
+		if (path.empty())
+		{
+			return Si::none;
+		}
+		auto digest_begin = path.begin();
+		if (*digest_begin == '/')
+		{
+			++digest_begin;
+		}
+		content_request request;
+		auto const rest = decode_ascii_hex_bytes(digest_begin, path.end(), std::back_inserter(request.digest));
+		if (rest.first != path.end())
+		{
+			return Si::none;
+		}
+		return std::move(request);
 	}
 
 	template <class MakeSender, class Shutdown>
@@ -33,6 +124,14 @@ namespace
 			return;
 		}
 
+		auto const request = parse_request_path(header->path);
+		if (!request)
+		{
+			//TODO: 404 or sth
+			return;
+		}
+
+		auto const &request_ = *request;
 		std::string const body = "Hello at " + header->path;
 
 		auto const try_send = [&yield, &make_sender](std::string const &data)
